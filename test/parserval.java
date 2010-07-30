34ccc03a8d30e862578fcb02890bfc2c4814bb45
Ramon Nogueira <ramon@echophase.com>
2010-07-30 12:56:56 +0200

    Original BYACC/J source code version 1.15

---
+++ test/parserval.java
@@ -0,0 +1,26 @@
+
+
+//########## SEMANTIC VALUES ##########
+public class parserval
+{
+public int ival;
+public double dval;
+public String sval;
+public Object obj;
+public parserval(int val)
+{
+  ival=val;
+}
+public parserval(double val)
+{
+  dval=val;
+}
+public parserval(String val)
+{
+  sval=val;
+}
+public parserval(Object val)
+{
+  obj=val;
+}
+}//end class
